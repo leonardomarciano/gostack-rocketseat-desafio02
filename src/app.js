27da0532e60e51116295c2a1abe15957b7af2f15
leonardomarciano <leonardo@marciano.io>
2020-04-09 19:00:48 -0300

    project(start): Sincronizando projeto com github

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,76 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(project)
+
+  return response.json(project)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, techs, url} = request.body;
+
+  try {
+    const repositorieIndex = repositories.findIndex(e => e.id === id)
+    repositorieIndex === -1 ? (function(){throw "error"}()) : null
+    const repositorie = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[repositorieIndex].likes
+    }
+    repositories[repositorieIndex] = repositorie
+    return response.json(repositories[repositorieIndex])  
+  } catch (error) {
+
+    return response.status(400).json({error: "Repositorie not found", msg: error})    
+
+  }
+  
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const {id} = req.params;
+
+  try {
+    const repositorieIndex = repositories.findIndex(e => e.id === id)
+    repositorieIndex === -1 ? (function(){throw "error"}()) : null
+    repositories.splice(repositorieIndex, 1)
+    return res.status(204).send()
+  } catch (error) {
+    return res.status(400).json({error: "Repositorie not found", msg: error})    
+  }
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  try {
+    const repositorieIndex = repositories.findIndex(e => e.id === id)
+    repositorieIndex === -1 ? (function(){throw "error"}()) : null
+    repositories[repositorieIndex].likes += 1 
+
+    return response.json(repositories[repositorieIndex])
+  } catch (error) {
+    return response.status(400).json({error: "Repositorie not found", msg: error})    
+  }
+
 });
 
 module.exports = app;
